6421b2c449e03cb79ded0eadf8b9f677a4651859
Douézan-Grard Guillaume <silverwyrda@gmail.com>
2015-07-06 15:40:21 +0200

    Add vars

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 		cli.StringFlag{Name: "listen-host", Value: "0.0.0.0", Usage: "server listening host", EnvVar: "LISTEN_HOST"},
 		cli.IntFlag{Name: "listen-port", Value: 8080, Usage: "server listening port", EnvVar: "LISTEN_PORT"},
 
-		cli.StringFlag{Name: "sql-dialect", Value: "postgres", Usage: "database dialect ('sqlite' or 'postgres')", EnvVar: "SQL_DIALECT"},
+		cli.StringFlag{Name: "sql-dialect", Value: "sqlite3", Usage: "database dialect ('sqlite' or 'postgres')", EnvVar: "SQL_DIALECT"},
 
 		cli.StringFlag{Name: "postgres-host", Value: "postgres", Usage: "postgresql host", EnvVar: "POSTGRES_HOST"},
 		cli.IntFlag{Name: "postgres-port", Value: 5432, Usage: "postgresql port", EnvVar: "POSTGRES_PORT"},
@@ -48,6 +48,7 @@ func main() {
 	cmd.RunAndExitOnError()
 }
 
+// TODO: Add Ping to the database when not migrating
 func serve(ctx *cli.Context) error {
 	var app *application.Application
 	var err error
@@ -60,7 +61,7 @@ func serve(ctx *cli.Context) error {
 
 	switch ctx.String("sql-dialect") {
 	case "postgres":
-		dialect = "sqlite"
+		dialect = "postgres"
 		args = fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=verify-full",
 			ctx.String("postgres-user"),
 			ctx.String("postgres-password"),
@@ -69,17 +70,17 @@ func serve(ctx *cli.Context) error {
 			ctx.String("postgres-db"),
 		)
 		logs.Debug("Database %s at %s", dialect, ctx.String("postgres-host"))
-	case "sqlite":
+	case "sqlite3":
 		fallthrough
 	default:
-		dialect = "sqlite"
+		dialect = "sqlite3"
 		args = ctx.String("sqlite-path")
-		logs.Debug("Database %s at %s", dialect, args)
+		logs.Debug("Loading database %s at %s", dialect, args)
 	}
 
 	if ctx.Bool("migrate") {
 		migrate(dialect, args)
-		logs.Debug("Database migrated")
+		logs.Debug("Database migrated successfully")
 	}
 
 	app = application.New()
@@ -103,7 +104,7 @@ func serve(ctx *cli.Context) error {
 	app.Options("/contacts/:id", controllers.ContactOptions) // Required for CORS
 	app.Delete("/contacts/:id", controllers.DeleteContactByID)
 
-	return app.Serve(ctx.String("listen"))
+	return app.Serve(fmt.Sprintf("%s:%d", ctx.String("listen-host"), ctx.Int("listen-port")))
 }
 
 func cors(h http.Handler) http.Handler {
